06c8a426cbd672606a70ff2711c7e19bcf07e94d
eric <ericgaona0413@gmail.com>
2024-12-22 21:23:33 +0000

    adding the models

--- bank/views.py
+++ bank/views.py
@@ -4,6 +4,14 @@ from .models import User
 # Create your views here.
 
 def index(request):
+    # user = User.objects.get(username=request.user.username)  DA ERROR SI NO ESTA LOGUEADO
+    # users = User.objects.all()
+    # print(f"SOY user ---QuerySet--- >>> {users}")
+    # print(" ----- 1 -------------- ")
+    # users_json = User.objects.all().values()
+    # print(f"SOY user_json ----- >>> {users_json}")
+    # print(" ----- 2 -------------- ")
+
     test = "world!"
     return render(request, "bank/index.html", {
         "test": test
